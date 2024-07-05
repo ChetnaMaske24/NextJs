586968fd622c54eed35d7823ab11dba1d7b300ea
Chetna <cmaske580@gmail.com>
2024-07-05 20:24:31 +0530

    global and inline css

---
+++ next_tut/src/app/userStyle/page.js
@@ -0,0 +1,12 @@
+"use client"
+import './style.css';
+
+export default function UserStyle (){
+    return(
+        <div>
+            <h1>Style</h1>
+            <h2 style={{color:"purple"}}>global and inline Css</h2>
+            <p>Next.js is an open-source web development framework based on React and has gained significant popularity due to its amazing features.</p>
+        </div>
+    )
+}
